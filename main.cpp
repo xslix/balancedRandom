c7847dbe1854aeef30eced9ae225d5fc5ab5ccf6
Седякин Илья <isedyakin@cmx.ru>
2018-08-29 16:14:46 +0300

    second

--- main.cpp
+++ main.cpp
@@ -12,69 +12,62 @@ class balancedRandom {
     private: double koef;
     private: double savedPercent;
 
-    public: balancedRandom(double probabilityPercent = 0.0, double koefficient = 0.06)
-    {
-        srand (time(NULL));
+    public: balancedRandom(double probabilityPercent = 0.0, double koefficient = 0.06) {
+        srand(time(NULL));
         koef = koefficient;
         savedPercent = probabilityPercent;
     };
 
     public: bool getChance(double probabilityPercent) {
-      int randVal = rand() % 100000 + 1;
-      if (randVal <= probabilityPercent * (1 + abs(luck) / (abs(luck) + 1) * (luck > 0 ? 1 : -1)) * 1000)
-      {
-          luck -= (100 - probabilityPercent) * koef;
-          return true;
-      }
-      else
-      {
-          luck += probabilityPercent * koef;
-          return false;
-      }
+        int randVal = rand() % 100000 + 1;
+        if (randVal <= probabilityPercent * (1 + abs(luck) / (abs(luck) + 1) * (luck > 0 ? 1 : -1)) * 1000)
+        {
+            luck -= (100 - probabilityPercent) * koef;
+            return true;
+        }
+        else
+        {
+            luck += probabilityPercent * koef;
+            return false;
+        }
     }
 
     public: bool getChance() {
-      int randVal = rand() % 100000 + 1;
-      if (randVal <= savedPercent * (1 + abs(luck * koef) / (abs(luck* koef) + 1) * (luck > 0 ? 1 : -1)) * 1000)
-      {
-          luck -= (100 - savedPercent) * koef;
-          return true;
-      }
-      else
-      {
-          luck += savedPercent * koef;
-          return false;
-      }
+        int randVal = rand() % 100000 + 1;
+        if (randVal <= savedPercent * (1 + abs(luck) / (abs(luck) + 1) * (luck > 0 ? 1 : -1)) * 1000)
+        {
+            luck -= (100 - savedPercent) * koef;
+            return true;
+        }
+        else
+        {
+            luck += savedPercent * koef;
+            return false;
+        }
     }
 
-    public: getLuck()
-    {
+    public: getLuck() {
         return luck;
     }
 
-    public: setLuck(double value)
-    {
+    public: setLuck(double value) {
         luck = value;
     }
 
-    public: getKoef()
-    {
+    public: getKoef() {
         return koef;
     }
 
-    public: setKoef(double value)
-    {
+    public: setKoef(double value) {
         koef = value;
     }
 
-    public: getPercent()
-    {
+    public: getPercent() {
         return savedPercent;
     }
 
-    public: setPercent(double value)
-    {
-        percent = value;
+    public: setPercent(double value) {
+        savedPercent = value;
     }
 
 
